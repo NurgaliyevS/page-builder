1814690d248cbef1bd0182f3d58846977e0e6c47
NURGASAB <sabyr.nurgaliyev@bcc.kz>
2024-08-31 15:50:11 +0500

    upd

--- project.custom.config.js
+++ project.custom.config.js
@@ -5,44 +5,44 @@ export const customConfig = {
     theme: "winter",
     main: themes[`[data-theme=winter"]`],
   },
-  // example as pregnantmeal.com without https://
-  domainName: "pregnantmeal.com",
+  // example as subpage.io without https://
+  domainName: "subpage.io",
   mailgun: {
     subdomain: "mg",
-    fromNoReply: `PregnantMeal <noreply@mg.pregnantmeal.com>`,
-    fromAdmin: `PregnantMeal <admin@mg.pregnantmeal.com>`,
-    supportEmail: "support@pregnantmeal.com",
+    fromNoReply: `SubPage <noreply@mg.subpage.io>`,
+    fromAdmin: `SubPage <admin@mg.subpage.io>`,
+    supportEmail: "support@subpage.io",
     forwardRepliesTo: "nurgasab@gmail.com",
   },
-  documentTitle: "Yummy Meals, Strong Baby - PregnantMeal",
-  domainWithHttps: "https://pregnantmeal.com",
+  documentTitle: "Yummy Meals, Strong Baby - SubPage",
+  domainWithHttps: "https://subpage.io",
   seo: {
     keywords:
       "first trimester recipes, 7 day meal plan for pregnant woman, pregnancy dinner recipes first trimester, dinner ideas for first trimester, meal plans for pregnant moms",
     description:
       "Yummy Meals, Strong Baby! Get personalized daily, weekly, and monthly meal plans for a healthy pregnancy. Eat smart and grow a happy baby with our easy-to-use food guide.",
     themeColor: "#ffffff",
-    applicationName: "pregnantmeal",
+    applicationName: "SubPage",
     og: {
-      title: "PregnantMeal - Yummy Meals, Strong Baby!",
-      url: "https://pregnantmeal.com",
-      image: "https://pregnantmeal.com/company_related/og-image.webp",
+      title: "SubPage - Yummy Meals, Strong Baby!",
+      url: "https://subpage.io",
+      image: "https://subpage.io/company_related/og-image.webp",
       imageAlt:
-        "PregnantMeal - Yummy Meals, Strong Baby! Get personalized daily, weekly, and monthly meal plans for a healthy pregnancy. Eat smart and grow a happy baby with our easy-to-use food guide.",
+        "SubPage - Yummy Meals, Strong Baby! Get personalized daily, weekly, and monthly meal plans for a healthy pregnancy. Eat smart and grow a happy baby with our easy-to-use food guide.",
       content: "https://x.com/tech_nurgaliyev",
       twitterSite: "@tech_nurgaliyev",
-      twitterImage: "https://pregnantmeal.com/company_related/og-image.webp",
+      twitterImage: "https://subpage.io/company_related/og-image.webp",
     },
   },
   blog: {
-    title: "PregnantMeal Blog",
+    title: "SubPage Blog",
     description:
       "Foods to avoid during pregnancy, first trimester recipes, 7 day meal plan for pregnant woman, pregnancy food aversions, pregnancy dinner recipes first trimester, dinner ideas for first trimester, meal plans for pregnant moms",
-    canonical: "https://pregnantmeal.com/blog",
+    canonical: "https://subpage.io/blog",
     author: {
       name: "Sabyr Nurgaliyev",
       description:
-        "I am a software engineer and the husband of a pregnant woman. I wanted to create a platform that helps pregnant women because I know how hard it is to find the right information. My wife is pregnant, and I want to make sure she is eating the right food. I am building PregnantMeal to help moms eat healthy and grow a happy baby.",
+        "I am a software engineer and the husband of a pregnant woman. I wanted to create a platform that helps pregnant women because I know how hard it is to find the right information. My wife is pregnant, and I want to make sure she is eating the right food. I am building SubPage to help moms eat healthy and grow a happy baby.",
     },
   },
 };
