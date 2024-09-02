2dbc30c044aa8ddf10d178d86ba8744eca294f97
NURGASAB <sabyr.nurgaliyev@bcc.kz>
2024-09-02 20:57:50 +0500

    upd

--- components/Main.jsx
+++ components/Main.jsx
@@ -4,7 +4,7 @@ import CTAButton from "./CTAButton";
 function Main() {
   const plausible = usePlausible();
   return (
-    <section className="container max-w-7xl mx-auto flex flex-col items-center justify-between px-8 py-8 lg:py-20 gap-10">
+    <section className="container max-w-7xl mx-auto flex flex-col items-center justify-between px-8 py-8 gap-10">
       <div className="flex flex-col lg:flex-row items-center lg:items-start justify-between w-full gap-16">
         <div className="flex flex-col gap-10 lg:gap-14 items-center lg:items-start text-center lg:text-left w-full lg:w-2/3">
           <h1 className="font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4 flex justify-center items-center">
@@ -23,7 +23,7 @@ function Main() {
           <CTAButton plausibleNameBeforeLogin="GET_STARTED_MAIN" />
         </div>
 
-        <div className="hidden lg:block relative max-md:-m-4 lg:w-1/2">
+        <div className="relative max-md:-m-4">
           <div className="mockup-phone">
             <div className="camera"></div>
             <div className="display">
