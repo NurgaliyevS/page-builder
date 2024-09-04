9de8d36925b1eae4dc554bf202b44d8968841633
NURGASAB <sabyr.nurgaliyev@bcc.kz>
2024-09-04 15:32:23 +0500

    upd

--- components/Ads2.jsx
+++ components/Ads2.jsx
@@ -6,17 +6,8 @@ function Ads2() {
   return (
     <section className="bg-neutral p-6 py-16 md:py-32 text-gray-300">
       <div className="mx-auto flex container max-w-7xl items-center justify-center flex-col gap-8 md:flex-row md:gap-16">
-        <div className="max-w-lg space-y-6 md:space-y-8">
-          <h2 className="text-3xl md:text-5xl font-bold">
-            Create your landing page
-          </h2>
-          <p className="leading-relaxed">
-            Your Audience is Waiting. Start Building Today!
-          </p>
-          <CTAButton />
-        </div>
         <video
-          className="aspect-square w-full rounded-3xl border-2 border-base-content/20 sm:w-96 md:border-4"
+          className="aspect-square w-full rounded-3xl border-2 border-base-content/20 sm:w-96 md:border-4 md:order-last"
           autoPlay
           muted
           loop
@@ -34,6 +25,15 @@ function Ads2() {
             type="video/mp4"
           /> */}
         </video>
+        <div className="max-w-lg space-y-6 md:space-y-8">
+          <h2 className="text-3xl md:text-5xl font-bold">
+            Create your landing page
+          </h2>
+          <p className="leading-relaxed">
+            Your Audience is Waiting. Start Building Today!
+          </p>
+          <CTAButton />
+        </div>
       </div>
     </section>
   );
