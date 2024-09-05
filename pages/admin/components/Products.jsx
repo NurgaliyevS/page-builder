fe551d88c7e778c9d65062b21869b61bba2c653f
NURGASAB <sabyr.nurgaliyev@bcc.kz>
2024-09-05 15:59:23 +0500

    upd

---
+++ pages/admin/components/Products.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Products(props) {
+    return (
+        <div className='flex'>
+            <button className='btn btn-primary btn-block gap-2'>
+                Add products
+            </button>
+        </div>
+    );
+}
+
+export default Products;
--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -3,6 +3,7 @@ import HeaderAdmin from "./components/HeaderAdmin";
 import Constructor from "./components/Constructor";
 import PhoneMockup from "./components/PhoneMockup";
 import { useSession } from "next-auth/react";
+import Products from "./components/Products";
 
 function Admin() {
   const { data: session } = useSession();
@@ -25,12 +26,12 @@ function Admin() {
   useEffect(() => {
     if (!isInitialMount.current) {
       if (session?.user) {
-        setPageContent(prevContent => ({
+        setPageContent((prevContent) => ({
           ...prevContent,
           userName: session.user.name || "",
           userImage: session.user.image || "",
         }));
-      isInitialMount.current = true;
+        isInitialMount.current = true;
       }
     }
   }, [session]);
@@ -45,7 +46,9 @@ function Admin() {
       <main className="container mx-auto p-4 lg:p-8 max-w-7xl">
         <div className="flex flex-col lg:flex-row gap-8 max-w-full">
           <div className="flex-1">
-            <h1 className="text-3xl font-bold mb-6 text-gray-300">Page Constructor</h1>
+            <h1 className="text-3xl font-bold mb-6 text-gray-300">
+              Page Constructor
+            </h1>
             <div className="card bg-base-100 shadow-xl">
               <div className="card-body">
                 <Constructor onUpdate={handleUpdate} content={pageContent} />
@@ -53,8 +56,21 @@ function Admin() {
             </div>
           </div>
           <div className="flex-1/2">
-            <h2 className="text-2xl font-semibold mb-4 text-gray-300">Preview</h2>
+            <h2 className="text-2xl font-semibold mb-4 text-gray-300">
+              Preview
+            </h2>
             <PhoneMockup content={pageContent} />
+          </div>
+        </div>
+
+        <div className="flex flex-col lg:flex-row gap-8 max-w-full">
+          <div className="flex-1">
+            <div className="w-2/3">
+              <h1 className="text-3xl font-bold mb-6 text-gray-300">
+                Product Constructor
+              </h1>
+              <Products />
+            </div>
           </div>
         </div>
       </main>
