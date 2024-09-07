319d5900ff4c3d58c7fc98b9e04b52c8098e31c4
NURGASAB <sabyr.nurgaliyev@bcc.kz>
2024-09-07 19:21:01 +0500

    upd

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -8,7 +8,7 @@ import Products from "./components/Products";
 function Admin() {
   const { data: session } = useSession();
 
-  console.log(session, 'session')
+  console.log(session, "session");
 
   const [pageContent, setPageContent] = useState({
     ctaButtonText: "Subscribe",
@@ -68,6 +68,11 @@ function Admin() {
     setAccordionState((prev) => ({ ...prev, [section]: !prev[section] }));
   };
 
+  const handleSubmit = () => {
+    // TODO: Implement submission logic here
+    console.log("Submitting data:", { pageContent, productContent });
+  };
+
   return (
     <div className="min-h-screen bg-gray-100">
       <HeaderAdmin />
@@ -77,12 +82,12 @@ function Admin() {
             <div className="card-body">
               <button
                 className="text-xl font-bold mb-2 w-full text-left flex justify-between items-center"
-                onClick={() => toggleAccordion('constructor')}
+                onClick={() => toggleAccordion("constructor")}
               >
                 Page Constructor
                 <svg
                   className={`w-6 h-6 transform transition-transform ${
-                    accordionState.constructor ? 'rotate-180' : ''
+                    accordionState.constructor ? "rotate-180" : ""
                   }`}
                   fill="none"
                   stroke="currentColor"
@@ -108,7 +113,7 @@ function Admin() {
               <button
                 className="text-xl font-bold mb-2 w-full text-left flex justify-between items-center"
                 onClick={() => {
-                  toggleAccordion('products')
+                  toggleAccordion("products");
                   setProductContent((prevContent) => ({
                     ...prevContent,
                     isOpenProduct: !accordionState.products,
@@ -118,7 +123,7 @@ function Admin() {
                 Product Constructor
                 <svg
                   className={`w-6 h-6 transform transition-transform ${
-                    accordionState.products ? 'rotate-180' : ''
+                    accordionState.products ? "rotate-180" : ""
                   }`}
                   fill="none"
                   stroke="currentColor"
@@ -141,11 +146,15 @@ function Admin() {
               )}
             </div>
           </div>
+
+          <button className="btn btn-primary w-full" onClick={handleSubmit}>
+            Submit
+          </button>
         </div>
 
         <div className="hidden md:block md:basis-2/5 mx-auto max-w-sm">
           <h2 className="text-2xl font-semibold mb-4">Preview</h2>
-          <PhoneMockup content={pageContent} product={productContent}/>
+          <PhoneMockup content={pageContent} product={productContent} />
         </div>
       </div>
     </div>
