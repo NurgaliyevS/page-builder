0ad25fdf4386621ec297a5e9e015893aa0d38654
NurgaliyevS <nurgasab@gmail.com>
2024-09-08 10:17:18 +0500

    upd

--- pages/admin/components/HeaderAdmin.jsx
+++ pages/admin/components/HeaderAdmin.jsx
@@ -53,48 +53,6 @@ const HeaderAdmin = () => {
               <div className="text-sm">Style</div>
             </div>
           </Link>
-          <Link href="/admin/analytics" passHref>
-            <div
-              className={`btn-ghost btn-sm btn gap-1.5 lg:btn-md ${
-                router.pathname === "/admin/analytics" ? "btn-active" : ""
-              }`}
-            >
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="h-5 w-5"
-                >
-                  <path d="M15.5 2A1.5 1.5 0 0014 3.5v13a1.5 1.5 0 001.5 1.5h1a1.5 1.5 0 001.5-1.5v-13A1.5 1.5 0 0016.5 2h-1zM9.5 6A1.5 1.5 0 008 7.5v9A1.5 1.5 0 009.5 18h1a1.5 1.5 0 001.5-1.5v-9A1.5 1.5 0 0010.5 6h-1zM3.5 10A1.5 1.5 0 002 11.5v5A1.5 1.5 0 003.5 18h1A1.5 1.5 0 006 16.5v-5A1.5 1.5 0 004.5 10h-1z"></path>
-                </svg>
-              </span>
-              <div className="text-sm">Stats</div>
-            </div>
-          </Link>
-          <Link href="/admin/settings" passHref>
-            <div
-              className={`btn-ghost btn-sm btn gap-1.5 lg:btn-md ${
-                router.pathname === "/admin/settings" ? "btn-active" : ""
-              }`}
-            >
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="h-5 w-5"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M7.84 1.804A1 1 0 018.82 1h2.36a1 1 0 01.98.804l.331 1.652a6.993 6.993 0 011.929 1.115l1.598-.54a1 1 0 011.186.447l1.18 2.044a1 1 0 01-.205 1.251l-1.267 1.113a7.047 7.047 0 010 2.228l1.267 1.113a1 1 0 01.206 1.25l-1.18 2.045a1 1 0 01-1.187.447l-1.598-.54a6.993 6.993 0 01-1.929 1.115l-.33 1.652a1 1 0 01-.98.804H8.82a1 1 0 01-.98-.804l-.331-1.652a6.993 6.993 0 01-1.929-1.115l-1.598.54a1 1 0 01-1.186-.447l-1.18-2.044a1 1 0 01.205-1.251l1.267-1.114a7.05 7.05 0 010-2.227L1.821 7.773a1 1 0 01-.206-1.25l1.18-2.045a1 1 0 011.187-.447l1.598.54A6.993 6.993 0 017.51 3.456l.33-1.652zM10 13a3 3 0 100-6 3 3 0 000 6z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </span>
-              <div className="text-sm">Settings</div>
-            </div>
-          </Link>
         </div>
         <div className="flex-none">
           <Link href="/admin/upgrade" passHref>
