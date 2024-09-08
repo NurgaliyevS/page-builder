299b8c8bd0ea541eb5213fb554a3bba72d11c032
NurgaliyevS <nurgasab@gmail.com>
2024-09-08 17:57:49 +0500

    upd

--- pages/admin/style/index.jsx
+++ pages/admin/style/index.jsx
@@ -90,6 +90,11 @@ function Style() {
   useEffect(() => {
     if (isInitialMount.current) {
       if (session?.user) {
+        setPageContent((prevContent) => ({
+          ...prevContent,
+          userName: session.user.name || "",
+          userImage: session.user.image || "",
+        }));
         fetchLandingPage();
         isInitialMount.current = false;
       }
