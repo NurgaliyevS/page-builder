adada08560ef40a4d5c8517deb6968a619c2c537
NurgaliyevS <nurgasab@gmail.com>
2024-09-09 17:30:08 +0500

    upd

--- pages/[personalLink].js
+++ pages/[personalLink].js
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import clientPromise from '../backend/mongodbClient';
-import Image from 'next/image';
-import Link from 'next/link';
+import { useState } from "react";
+import clientPromise from "../backend/mongodbClient";
+import Image from "next/image";
+import Link from "next/link";
 
 export async function getServerSideProps(context) {
   const { personalLink } = context.params;
@@ -10,7 +10,9 @@ export async function getServerSideProps(context) {
     const client = await clientPromise;
     const db = client.db();
 
-    const landingPage = await db.collection('landingpages').findOne({ personalLink });
+    const landingPage = await db
+      .collection("landingpages")
+      .findOne({ personalLink });
 
     if (!landingPage) {
       return { notFound: true };
@@ -22,27 +24,50 @@ export async function getServerSideProps(context) {
       },
     };
   } catch (error) {
-    console.error('Error fetching landing page:', error);
+    console.error("Error fetching landing page:", error);
     return { notFound: true };
   }
 }
 
 function LandingPageTemplate({ landingPage }) {
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     // Implement email submission logic here
   };
 
-  const hasProducts = landingPage?.content?.products && landingPage?.content?.products.length > 0;
+  const hasProducts = landingPage?.content?.products?.some(
+    (product) => product?.productName && product?.productURL
+  );
 
   return (
-    <main className="min-h-screen" data-theme={landingPage?.customizations?.theme} style={{ fontFamily: landingPage?.customizations?.font }}>
-      <div className="relative min-h-screen bg-base-200" data-theme={landingPage?.customizations?.theme}>
-        <div className={`mx-auto flex min-h-screen w-full flex-col max-lg:pb-16 ${hasProducts ? 'lg:flex-row' : 'items-center justify-center'}`}>
-          <section className={`shrink-0 space-y-4 p-6 ${hasProducts ? 'lg:w-1/4 lg:space-y-8 lg:p-16 lg:pr-0 xl:w-1/3 xl:pr-16' : 'w-full max-w-2xl'}`}>
-            <div className={`flex items-start justify-start gap-4 lg:flex-col lg:gap-8 ${!hasProducts ? 'items-center' : ''}`}>
+    <main
+      className="min-h-screen"
+      data-theme={landingPage?.customizations?.theme}
+      style={{ fontFamily: landingPage?.customizations?.font }}
+    >
+      <div
+        className="relative min-h-screen bg-base-200"
+        data-theme={landingPage?.customizations?.theme}
+      >
+        <div
+          className={`mx-auto flex min-h-screen w-full flex-col max-lg:pb-16 ${
+            hasProducts ? "lg:flex-row" : "items-center justify-center"
+          }`}
+        >
+          <section
+            className={`shrink-0 space-y-4 p-6 ${
+              hasProducts
+                ? "lg:w-1/4 lg:space-y-8 lg:p-16 lg:pr-0 xl:w-1/3 xl:pr-16"
+                : "w-full max-w-2xl"
+            }`}
+          >
+            <div
+              className={`flex items-start justify-start gap-4 lg:flex-col lg:gap-8 ${
+                !hasProducts ? "items-center" : ""
+              }`}
+            >
               {landingPage?.content?.showUserIcon && (
                 <span className="relative shrink-0">
                   <img
@@ -55,25 +80,44 @@ function LandingPageTemplate({ landingPage }) {
                   <div className="absolute inset-0 rounded-full shadow-[0_0_0px_1px_rgba(0,0,0,0.06)]"></div>
                 </span>
               )}
-              <div className={`flex-1 ${!hasProducts ? 'text-center' : ''}`}>
-                <h1 className={`mb-1 text-xl font-bold lg:mb-3 lg:text-4xl lg:font-extrabold ${!hasProducts ? 'mx-auto' : ''}`}>{landingPage?.content?.mainHeadline}</h1>
+              <div className={`flex-1 ${!hasProducts ? "text-center" : ""}`}>
+                <h1
+                  className={`mb-1 text-xl font-bold lg:mb-3 lg:text-4xl lg:font-extrabold ${
+                    !hasProducts ? "mx-auto" : ""
+                  }`}
+                >
+                  {landingPage?.content?.mainHeadline}
+                </h1>
                 <div className="flex flex-col gap-1 lg:flex-row lg:gap-4"></div>
               </div>
             </div>
-            <div className={`reactMarkDown -space-y-4 leading-relaxed lg:text-lg ${!hasProducts ? 'text-center' : ''}`}>
+            <div
+              className={`reactMarkDown -space-y-4 leading-relaxed lg:text-lg ${
+                !hasProducts ? "text-center" : ""
+              }`}
+            >
               <p>{landingPage?.content?.mainDescription}</p>
             </div>
             {landingPage?.content?.showEmailInput && (
               <form onSubmit={handleSubmit}>
-                <input
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="input input-bordered w-full"
-                  placeholder="example@gmail.com"
-                />
+                <div className="form-control w-full">
+                  <label className="label">
+                    <span className="label-text">
+                      {landingPage?.content?.emailInputValue}
+                    </span>
+                  </label>
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="input input-bordered w-full"
+                    placeholder="example@gmail.com"
+                  />
+                </div>
                 {landingPage?.content?.showCTAButton && (
-                  <button type="submit" className="btn btn-primary mt-2 w-full">{landingPage?.content?.ctaText}</button>
+                  <button type="submit" className="btn btn-primary mt-2 w-full">
+                    {landingPage?.content?.ctaButtonText}
+                  </button>
                 )}
               </form>
             )}
@@ -82,19 +126,32 @@ function LandingPageTemplate({ landingPage }) {
             <section className="noscrollbar w-full lg:h-screen lg:overflow-scroll">
               <div className="divider my-0 px-6 lg:hidden"></div>
               <ul className="p-6 max-lg:space-y-4 lg:grid lg:grid-cols-2 lg:gap-8 lg:p-16">
-                {landingPage.content.products.map((product, index) => (
-                  <li key={index} className="card h-min bg-base-100 duration-200 col-span-1">
-                    <Link href={product?.productURL} target="_blank" className="group rounded-box cursor-pointer p-4 duration-200 hover:scale-[1.02] hover:bg-base-300 lg:p-6 space-y-1 lg:space-y-2">
-                      <div className="flex flex-wrap items-center gap-y-1 gap-x-2 lg:gap-x-3">
-                        <p className="mr-auto font-bold lg:text-lg">{product?.productName}</p>
-                        <div className="flex gap-2">
-                          <span className="badge badge-primary badge-sm lg:badge-md whitespace-nowrap duration-200">
-                            {product?.productStage}
-                          </span>
+                {landingPage?.content?.products?.map((product, index) => (
+                  <li
+                    key={index}
+                    className="card h-min bg-base-100 duration-200 col-span-1"
+                  >
+                    {product?.productName && product?.productURL && (
+                      <Link
+                        href={product?.productURL}
+                        target="_blank"
+                        className="group rounded-box cursor-pointer p-4 duration-200 hover:scale-[1.02] hover:bg-base-300 lg:p-6 space-y-1 lg:space-y-2"
+                      >
+                        <div className="flex flex-wrap items-center gap-y-1 gap-x-2 lg:gap-x-3">
+                          <p className="mr-auto font-bold lg:text-lg">
+                            {product?.productName}
+                          </p>
+                          <div className="flex gap-2">
+                            <span className="badge badge-primary badge-sm lg:badge-md whitespace-nowrap duration-200">
+                              {product?.productStage}
+                            </span>
+                          </div>
                         </div>
-                      </div>
-                      <p className="text-base-content/80 text-sm lg:text-base">{product?.productDescription}</p>
-                    </Link>
+                        <p className="text-base-content/80 text-sm lg:text-base">
+                          {product?.productDescription}
+                        </p>
+                      </Link>
+                    )}
                   </li>
                 ))}
               </ul>
