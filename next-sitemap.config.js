3fd6a151a3ba08c4802cefe1c2213c3154e84b1e
NurgaliyevS <nurgasab@gmail.com>
2024-09-21 20:31:53 +0500

    upd

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://nurgaliyevs.com",
+  siteUrl: "https://subpage.io",
   generateRobotsTxt: true,
   exclude: ["/admin", "/admin/*", "/blocked"],
   additionalPaths: async (config) => {
